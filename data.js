e5abba3dd3f6131b65101f6d2b7d4bc1ce359cfe
Ellen Corcoran-Walsh <54degreesdesign@gmail.com>
2017-03-14 14:14:30 +0000

    Residential Tour with FLoor Plans

---
+++ data.js
@@ -0,0 +1,339 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-of-house",
+      "name": "Front of House",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1265644537370374,
+        "pitch": -0.01185819794695675,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.412410854294758,
+          "pitch": 0.21102443977849283,
+          "rotation": 0,
+          "target": "1-entrance-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrance-hall",
+      "name": "Entrance Hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.7939049275995504,
+        "pitch": -0.03356912625636532,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5502390825122987,
+          "pitch": 0.7943797236077348,
+          "rotation": 0,
+          "target": "0-front-of-house"
+        },
+        {
+          "yaw": 2.123327441869586,
+          "pitch": 0.30326078355006203,
+          "rotation": 0,
+          "target": "2-sittingroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sittingroom",
+      "name": "Sittingroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5335340341707653,
+        "pitch": 0.247677936433643,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7796453536365817,
+          "pitch": 0.49403727622182103,
+          "rotation": 0,
+          "target": "1-entrance-hall"
+        },
+        {
+          "yaw": 2.6453522812504895,
+          "pitch": 0.16128175770313646,
+          "rotation": 0,
+          "target": "3-sun-lounge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sun-lounge",
+      "name": "Sun Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.13286572583538,
+        "pitch": 0.1300763730516401,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.846645757647714,
+          "pitch": 0.14609264090677598,
+          "rotation": 0,
+          "target": "2-sittingroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "map-0-front-of-house",
+      "name": "Front of House",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1265644537370374,
+        "pitch": -0.01185819794695675,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.412410854294758,
+          "pitch": 0.21102443977849283,
+          "rotation": 0,
+          "target": "1-entrance-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "map-1-entrance-hall",
+      "name": "Entrance Hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.7939049275995504,
+        "pitch": -0.03356912625636532,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5502390825122987,
+          "pitch": 0.7943797236077348,
+          "rotation": 0,
+          "target": "0-front-of-house"
+        },
+        {
+          "yaw": 2.123327441869586,
+          "pitch": 0.30326078355006203,
+          "rotation": 0,
+          "target": "2-sittingroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "map-2-sittingroom",
+      "name": "Sittingroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5335340341707653,
+        "pitch": 0.247677936433643,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7796453536365817,
+          "pitch": 0.49403727622182103,
+          "rotation": 0,
+          "target": "1-entrance-hall"
+        },
+        {
+          "yaw": 2.6453522812504895,
+          "pitch": 0.16128175770313646,
+          "rotation": 0,
+          "target": "3-sun-lounge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "map-3-sun-lounge",
+      "name": "Sun Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.13286572583538,
+        "pitch": 0.1300763730516401,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.846645757647714,
+          "pitch": 0.14609264090677598,
+          "rotation": 0,
+          "target": "2-sittingroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Property for Sale",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
